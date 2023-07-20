370476ef4ae4fe5e2092a1166142bc8dd459f8ec
Katkar <vivek.katkar@capgemini.com>
2023-07-20 16:59:09 +0530

    updated new

---
+++ src/main/java/Automation/Preperfly/App - Copy.java
@@ -0,0 +1,13 @@
+package Automation.Preperfly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
